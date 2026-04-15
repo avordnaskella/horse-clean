c684b91d255cc3b2067d3ec2f68a7f26b252a9e0
avordnaskella <avordnaskella@gmail.com>
2026-04-15 21:19:51 +0300

    изменения для добавления стилей

--- first_project/settings.py
+++ first_project/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEBUG = False  # на Render должно быть False
--- first_project/urls.py
+++ first_project/urls.py
@@ -23,3 +23,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('module_project.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Добавляем для обслуживания статики и медиа в продакшене
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Для продакшена через WhiteNoise
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
