645ec357f9b950df1f53748c7d50b3b483d0aae6
avordnaskella <avordnaskella@gmail.com>
2026-04-18 23:28:05 +0300

    для сп возможность добавления

--- module_project/admin.py
+++ module_project/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Discipline
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+@admin.register(Discipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'difficulty', 'is_olympic', 'created_at')
+    list_filter = ('category', 'difficulty', 'is_olympic')
+    search_fields = ('name', 'description')
